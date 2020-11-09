7a0b518a65a21d78447078395f012e4d81cca1a7
Meet Patel <71483201+Meetpatel-windsor@users.noreply.github.com>
2020-11-09 18:31:05 +0530

    Add files via upload

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -7,9 +7,27 @@ using System.Web.UI.WebControls;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
+   
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            if (Session["Id"] != null)
+            {
 
+                lblname1.Text = Session["Name"].ToString();
+                lblname2.Text = Session["Name"].ToString();
+
+                divprofile.Visible = true;
+                divsignin.Visible = false;
+
+            }
+            else
+            {
+                divprofile.Visible = false;
+                divsignin.Visible = true;
+            }
+        }
     }
 
     protected void btndashboard_Click(object sender, EventArgs e)
@@ -19,7 +37,45 @@ public partial class MasterPage : System.Web.UI.MasterPage
 
     protected void btnpost_Click(object sender, EventArgs e)
     {
-        Response.Redirect("Post.aspx");
+        if(Session["Id"]!=null)
+             Response.Redirect("Post.aspx");
+        else
+            Response.Redirect("Login.aspx");
 
+    }
+
+    protected void btnprofile_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Profile.aspx");
+    }
+
+    protected void btnmypost_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("ViewPost.aspx");
+
+    }
+
+    protected void btnlogin_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Dashboard.aspx");
+    }
+
+    protected void btnlogout_Click(object sender, EventArgs e)
+    {
+        Session["Id"] = null;
+        Session["Name"] = null;
+
+        Response.Redirect("Dashboard.aspx");
+       }
+
+    protected void btnsignin_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Login.aspx");
+    }
+
+    protected void btnabout_Click(object sender, EventArgs e)
+    {
+        
+        Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "popup", "window.open('About/SS_Documentation.pdf' ,'_blank');", true);
     }
 }
